3e92df4c3349d521cf150fa3d4149381802e2b40
FelipeRey97 <andresfrey97@gmail.com>
2025-06-07 22:38:58 -0500

    Docker-compose.yml

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -5,13 +5,7 @@ host="$1"
 shift
 cmd="$@"
 
-echo "Esperando a que MySQL ($host) esté disponible..."
-echo "Usando credenciales: $DB_USERNAME / $DB_PASSWORD"
-
-until mysql -h "$host" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e 'SELECT 1' >/dev/null 2>&1; do
-  echo "MySQL no está listo, esperando 3 segundos..."
-  sleep 3
-done
+sleep 30
 
 echo "MySQL está listo, ejecutando comando: $cmd"
 exec $cmd
